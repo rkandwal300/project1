b2852ed0d191b81670bc67b70287bda1e89d2edc
Mayank Sinha <mayanksinha945@gmail.com>
2025-06-10 16:41:55 +0530

    feat(utilis): added logo and region

---
+++ src/lib/region.js
@@ -0,0 +1,247 @@
+const providerData = {
+
+    cloud: {
+        name: "Cloud",
+        list: {
+
+            aws: {
+
+                name: "AWS",
+                logo: "",
+
+                regions: {
+
+                    "af-south-1": [
+                        "c5.12xlarge",
+                        "c5.18xlarge",
+                        "c5.24xlarge",
+                        "c5.2xlarge",
+                        "c5.4xlarge",
+                    ],
+
+                    "ap-east-1": [
+                        "c5.12xlarge",
+                        "c5.18xlarge",
+                        "c5.24xlarge",
+                        "c5.2xlarge",
+                        "c5.4xlarge",
+                    ],
+
+                    "ap-northeast-1": [
+                        "a1.2xlarge",
+                        "a1.4xlarge",
+                        "a1.large",
+                        "a1.medium",
+                        "a1.metal",
+                    ],
+                    "ap-northeast-2": [
+                        "c4.2xlarge",
+                        "c4.4xlarge",
+                        "c4.8xlarge",
+                        "c4.large",
+                        "c4.xlarge",
+                    ],
+
+                    "ap-northeast-3": [
+                        "c4.2xlarge",
+                        "c4.4xlarge",
+                        "c4.8xlarge",
+                        "c4.large",
+                        "c4.xlarge"
+                    ],
+
+                },
+
+            },
+
+            azure: {
+                logo: "",
+                name: "AZURE",
+
+                regions: {
+
+                    australiacentral: [
+
+                        "standard_a1_v2",
+                        "standard_a2_v2",
+                        "standard_a2m_v2",
+                        "standard_a4_v2",
+                        "standard_a4m_v2",
+
+                    ],
+
+                    australiacentral2: [
+
+                        "standard_a1_v2",
+                        "standard_a2_v2",
+                        "standard_a2m_v2",
+                        "standard_a4_v2",
+                        "standard_a4m_v2",
+                    ],
+
+                    australiaeast: [
+
+                        "standard_a1_v2",
+                        "standard_a2_v2",
+                        "standard_a2m_v2",
+                        "standard_a4_v2",
+                        "standard_a4m_v2",
+
+                    ],
+
+                    australiasoutheast: [
+
+                        "standard_a1_v2",
+                        "standard_a2_v2",
+                        "standard_a2m_v2",
+                        "standard_a4_v2",
+                        "standard_a4m_v2",
+
+                    ],
+
+
+                    brazilsouth: [
+                        "standard_a1_v2",
+                        "standard_a2_v2",
+                        "standard_a2m_v2",
+                        "standard_a4_v2",
+                        "standard_a4m_v2",
+
+                    ],
+
+                },
+
+            },
+
+            gcp: {
+                logo: "",
+                name: "GCP",
+
+                regions: {
+
+                    "africa-south1": [
+
+                        "c4-highmem-16",
+                        "c4-highmem-192",
+                        "c4-highmem-2",
+                        "c4-highmem-32",
+                        "c4-highmem-4",
+                    ],
+
+                    "asia-east1": [
+                        "a3-highgpu-1g",
+                        "a3-highgpu-2g",
+                        "a3-highgpu-4g",
+                        "a3-highgpu-8g",
+                        "a3-megagpu-8g",
+                    ],
+
+                    "asia-east2": [
+
+                        "c2-standard-16",
+                        "c2-standard-30",
+                        "c2-standard-4",
+                        "c2-standard-60",
+                        "c2-standard-8",
+                    ],
+
+                    "asia-northeast1": [
+
+                        "a2-highgpu-1g",
+                        "a2-highgpu-2g",
+                        "a2-highgpu-4g",
+                        "a2-highgpu-8g",
+                        "a3-edgegpu-8g",
+
+                    ],
+
+                    "asia-northeast2": [
+                        "c2-standard-16",
+                        "c2-standard-30",
+                        "c2-standard-4",
+                        "c2-standard-60",
+                        "c2-standard-8",
+                    ],
+
+                },
+
+            },
+        },
+    },
+
+
+    telematry: {
+        datadog: {
+
+            name: "Datadog",
+
+            regions: {
+
+                "us-central1": ["us-east1", "us-west1", "us-east4", "us-west2"],
+
+                "europe-west1": ["europe-west2", "europe-west3", "europe-west4"],
+
+                "asia-east1": ["asia-northeast1", "asia-southeast1", "asia-south1"],
+
+            },
+        },
+        cloudWatch: {
+
+            name: "AWS CloudWatch",
+
+            regions: {
+
+                australiacentral: [
+
+                    "standard_a1_v2",
+                    "standard_a2_v2",
+                    "standard_a2m_v2",
+                    "standard_a4_v2",
+                    "standard_a4m_v2",
+
+                ],
+
+                australiacentral2: [
+
+                    "standard_a1_v2",
+                    "standard_a2_v2",
+                    "standard_a2m_v2",
+                    "standard_a4_v2",
+                    "standard_a4m_v2",
+                ],
+
+                australiaeast: [
+
+                    "standard_a1_v2",
+                    "standard_a2_v2",
+                    "standard_a2m_v2",
+                    "standard_a4_v2",
+                    "standard_a4m_v2",
+
+                ],
+
+                australiasoutheast: [
+
+                    "standard_a1_v2",
+                    "standard_a2_v2",
+                    "standard_a2m_v2",
+                    "standard_a4_v2",
+                    "standard_a4m_v2",
+
+                ],
+
+
+                brazilsouth: [
+                    "standard_a1_v2",
+                    "standard_a2_v2",
+                    "standard_a2m_v2",
+                    "standard_a4_v2",
+                    "standard_a4m_v2",
+
+                ],
+
+            },
+        }
+    }
+};
+
